34b4d1bdd4127fc77ed5954b12eeb3206495cb2c
Riko Airlan Ramadhan <rikoairlan57@gmail.com>
2023-07-15 11:48:17 +0700

    🔥 stater page

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div className="bg-black">
+      <h1 className="text-3xl font-bold underline text-center text-white">
+        Hello world!
+      </h1>
+    </div>
+  );
+}
+
+export default App;
